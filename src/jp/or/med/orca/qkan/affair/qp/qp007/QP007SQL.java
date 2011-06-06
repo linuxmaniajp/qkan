b5b378adb50365ca96118a2e19d4569df1810429
yoshida <yoshida@cvs.orca.med.or.jp>
2011-06-06 10:01:17 +0900

    Initial revision

---
+++ src/jp/or/med/orca/qkan/affair/qp/qp007/QP007SQL.java
@@ -0,0 +1,325 @@
+
+/*
+ * Project code name "ORCA"
+ * 給付管理台帳ソフト QKANCHO（JMA care benefit management software）
+ * Copyright(C) 2002 JMA (Japan Medical Association)
+ *
+ * This program is part of "QKANCHO (JMA care benefit management software)".
+ *
+ * This program is distributed in the hope that it will be useful
+ * for further advancement in medical care, according to JMA Open
+ * Source License, but WITHOUT ANY WARRANTY.
+ * Everyone is granted permission to use, copy, modify and
+ * redistribute this program, but only under the conditions described
+ * in the JMA Open Source License. You should have received a copy of
+ * this license along with this program. If not, stop using this
+ * program and contact JMA, 2-28-16 Honkomagome, Bunkyo-ku, Tokyo,
+ * 113-8621, Japan.
+ *****************************************************************
+ * アプリ: QKANCHO
+ * 開発者: 利用者向け印刷
+ * 作成日: 2010/11/12  日本コンピューター株式会社 利用者向け印刷 新規作成
+ * 更新日: ----/--/--
+ * システム 給付管理台帳 (Q)
+ * サブシステム 請求書出力 (P)
+ * プロセス 利用者向け印刷 (007)
+ * プログラム 利用者向請求書印刷設定 (QP007)
+ *
+ *****************************************************************
+ */
+package jp.or.med.orca.qkan.affair.qp.qp007;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.im.*;
+import java.text.*;
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.table.*;
+import jp.nichicom.ac.*;
+import jp.nichicom.ac.bind.*;
+import jp.nichicom.ac.component.*;
+import jp.nichicom.ac.component.dnd.*;
+import jp.nichicom.ac.component.dnd.event.*;
+import jp.nichicom.ac.component.event.*;
+import jp.nichicom.ac.component.mainmenu.*;
+import jp.nichicom.ac.component.table.*;
+import jp.nichicom.ac.component.table.event.*;
+import jp.nichicom.ac.container.*;
+import jp.nichicom.ac.core.*;
+import jp.nichicom.ac.filechooser.*;
+import jp.nichicom.ac.io.*;
+import jp.nichicom.ac.lang.*;
+import jp.nichicom.ac.pdf.*;
+import jp.nichicom.ac.sql.*;
+import jp.nichicom.ac.text.*;
+import jp.nichicom.ac.util.*;
+import jp.nichicom.ac.util.adapter.*;
+import jp.nichicom.vr.*;
+import jp.nichicom.vr.bind.*;
+import jp.nichicom.vr.bind.event.*;
+import jp.nichicom.vr.border.*;
+import jp.nichicom.vr.component.*;
+import jp.nichicom.vr.component.event.*;
+import jp.nichicom.vr.component.table.*;
+import jp.nichicom.vr.container.*;
+import jp.nichicom.vr.focus.*;
+import jp.nichicom.vr.image.*;
+import jp.nichicom.vr.io.*;
+import jp.nichicom.vr.layout.*;
+import jp.nichicom.vr.text.*;
+import jp.nichicom.vr.text.parsers.*;
+import jp.nichicom.vr.util.*;
+import jp.nichicom.vr.util.adapter.*;
+import jp.nichicom.vr.util.logging.*;
+import jp.or.med.orca.qkan.*;
+import jp.or.med.orca.qkan.affair.*;
+import jp.or.med.orca.qkan.component.*;
+
+/**
+ * 利用者向請求書印刷設定SQL定義(QP007) 
+ */
+public class QP007SQL extends QP007State {
+  private ACSQLSafeDateFormat dateFormat = new ACSQLSafeDateFormat();
+  /**
+   * コンストラクタです。
+   */
+  public QP007SQL() {
+  }
+
+  /**
+   * 「定型文情報取得」のためのSQLを返します。
+   * @param sqlParam SQL構築に必要なパラメタを格納したハッシュマップ
+   * @throws Exception 処理例外
+   * @return SQL文
+   */
+  public String getSQL_GET_FIXED_FORM(VRMap sqlParam) throws Exception{
+    StringBuffer sb = new StringBuffer();
+    Object[] inValues;
+    Stack conditionStack = new Stack(), conditionStackOfFrom = new Stack();
+    boolean firstCondition = true, firstConditionOfFrom = true;
+    Object obj;
+
+    sb.append("SELECT");
+
+    sb.append(" FIXED_FORM_ID");
+
+    sb.append(",CONTENT_KEY");
+
+    sb.append(",CONTENT");
+
+    sb.append(" FROM");
+
+    sb.append(" FIXED_FORM");
+
+    sb.append(" WHERE");
+
+    sb.append("(");
+
+    sb.append(" TABLE_TYPE");
+
+    sb.append(" =");
+
+    sb.append(" 3");
+
+    sb.append(")");
+
+    sb.append("AND");
+
+    sb.append("(");
+
+    sb.append(" PROVIDER_ID");
+
+    sb.append(" =");
+
+    sb.append(ACSQLSafeStringFormat.getInstance().format(VRBindPathParser.get("PROVIDER_ID", sqlParam)));
+
+    sb.append(")");
+
+    sb.append(" ORDER BY");
+
+    sb.append(" CONTENT_KEY");
+
+    sb.append(" ");
+
+    return sb.toString();
+  }
+
+  /**
+   * 「定型文登録」のためのSQLを返します。
+   * @param sqlParam SQL構築に必要なパラメタを格納したハッシュマップ
+   * @throws Exception 処理例外
+   * @return SQL文
+   */
+  public String getSQL_INSERT_FIXED_FORM(VRMap sqlParam) throws Exception{
+    StringBuffer sb = new StringBuffer();
+    Object[] inValues;
+    Stack conditionStack = new Stack(), conditionStackOfFrom = new Stack();
+    boolean firstCondition = true, firstConditionOfFrom = true;
+    Object obj;
+
+    sb.append("INSERT INTO");
+
+    sb.append(" FIXED_FORM");
+
+    sb.append("(");
+
+    sb.append(" TABLE_TYPE");
+
+    sb.append(",FIXED_FORM_ID");
+
+    sb.append(",CONTENT_KEY");
+
+    sb.append(",CONTENT");
+
+    sb.append(",CONTENT_SORT");
+
+    sb.append(",LAST_TIME");
+
+    sb.append(",PROVIDER_ID");
+
+    sb.append(")VALUES(");
+
+    sb.append(" 3");
+
+    sb.append(",");
+
+    sb.append(ACSQLSafeIntegerFormat.getInstance().format(VRBindPathParser.get("FIXED_FORM_ID", sqlParam)));
+
+    sb.append(",");
+
+    sb.append(ACSQLSafeIntegerFormat.getInstance().format(VRBindPathParser.get("CONTENT_KEY", sqlParam)));
+
+    sb.append(",");
+
+    sb.append(ACSQLSafeStringFormat.getInstance().format(VRBindPathParser.get("CONTENT", sqlParam)));
+
+    sb.append(",");
+
+    sb.append(" 0");
+
+    sb.append(",");
+
+    sb.append(" CURRENT_TIMESTAMP");
+
+    sb.append(",");
+
+    sb.append(ACSQLSafeStringFormat.getInstance().format(VRBindPathParser.get("PROVIDER_ID", sqlParam)));
+
+    sb.append(")");
+
+    return sb.toString();
+  }
+
+  /**
+   * 「」のためのSQLを返します。
+   * @param sqlParam SQL構築に必要なパラメタを格納したハッシュマップ
+   * @throws Exception 処理例外
+   * @return SQL文
+   */
+  public String getSQL_UPDATE_FIXED_FORM(VRMap sqlParam) throws Exception{
+    StringBuffer sb = new StringBuffer();
+    Object[] inValues;
+    Stack conditionStack = new Stack(), conditionStackOfFrom = new Stack();
+    boolean firstCondition = true, firstConditionOfFrom = true;
+    Object obj;
+
+    sb.append("UPDATE");
+
+    sb.append(" FIXED_FORM");
+
+    sb.append(" SET");
+
+    sb.append(" CONTENT");
+
+    sb.append(" =");
+
+    sb.append(ACSQLSafeStringFormat.getInstance().format(VRBindPathParser.get("CONTENT", sqlParam)));
+
+    sb.append(",");
+
+    sb.append(" LAST_TIME");
+
+    sb.append(" =");
+
+    sb.append(" CURRENT_TIMESTAMP");
+
+    sb.append(" WHERE");
+
+    sb.append("(");
+
+    sb.append(" TABLE_TYPE");
+
+    sb.append(" =");
+
+    sb.append(" 3");
+
+    sb.append(")");
+
+    sb.append("AND");
+
+    sb.append("(");
+
+    sb.append(" FIXED_FORM_ID");
+
+    sb.append(" =");
+
+    sb.append(ACSQLSafeIntegerFormat.getInstance().format(VRBindPathParser.get("FIXED_FORM_ID", sqlParam)));
+
+    sb.append(")");
+
+    sb.append("AND");
+
+    sb.append("(");
+
+    sb.append(" CONTENT_KEY");
+
+    sb.append(" =");
+
+    sb.append(ACSQLSafeIntegerFormat.getInstance().format(VRBindPathParser.get("CONTENT_KEY", sqlParam)));
+
+    sb.append(")");
+
+    return sb.toString();
+  }
+
+  /**
+   * 「定型文分類情報取得」のためのSQLを返します。
+   * @param sqlParam SQL構築に必要なパラメタを格納したハッシュマップ
+   * @throws Exception 処理例外
+   * @return SQL文
+   */
+  public String getSQL_GET_MAX_FIXED_FORM_ID(VRMap sqlParam) throws Exception{
+    StringBuffer sb = new StringBuffer();
+    Object[] inValues;
+    Stack conditionStack = new Stack(), conditionStackOfFrom = new Stack();
+    boolean firstCondition = true, firstConditionOfFrom = true;
+    Object obj;
+
+    sb.append("SELECT");
+
+    sb.append(" CASE WHEN MAX(FIXED_FORM_ID) IS NULL THEN 1 ELSE MAX(FIXED_FORM_ID) + 1 END AS MAX_FIXED_FORM_ID");
+
+    sb.append(" FROM");
+
+    sb.append(" FIXED_FORM");
+
+    sb.append(" WHERE");
+
+    sb.append("(");
+
+    sb.append(" TABLE_TYPE");
+
+    sb.append(" =");
+
+    sb.append(" 3");
+
+    sb.append(")");
+
+    return sb.toString();
+  }
+
+}
